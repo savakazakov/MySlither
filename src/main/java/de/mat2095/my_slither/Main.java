bfe26339b605939671e349e72fb8a2772bc9712b
notbasetwo <71050321+notbasetwo@users.noreply.github.com>
2021-10-20 22:58:51 +0100

    Create basic splash screen and start from main

--- src/main/java/de/mat2095/my_slither/Main.java
+++ src/main/java/de/mat2095/my_slither/Main.java
@@ -16,8 +16,8 @@ public final class Main {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
-
-        new MySlitherJFrame().setVisible(true);
+        
+        new Splash();
 
     }
 }
